58c39dddf74fc40392a28a1f91c26e310eb3a84e
Margarita Morozova <ritamorozova@yahoo.com>
2020-06-22 23:26:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  new_array = ["element1", "element2"]
-  new_array << "new_element"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
